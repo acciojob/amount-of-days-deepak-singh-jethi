df2c7713daedba02b94193c172f2a5a018287ef6
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-11-20 15:41:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let year = prompt("enter the year");
+
+function daysOfYear(year){
+	if((year%4==0 && year%100!==0) ||year%400==0){
+	console.log("366");
+}
+	else{
+		console.log("365");
+	}
+}
+}
